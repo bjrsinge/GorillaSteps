f5ef38e65f5a990a92e19f8e99dd993df6f981cc
bjrsinge2 <112808041+bjrsinge@users.noreply.github.com>
2024-07-21 16:26:12 -0230

    Added stats

--- Patches/Patch.cs
+++ Patches/Patch.cs
@@ -11,17 +11,18 @@ namespace GorillaSteps.Patches
         public static int stepsCount;
 
         static void Postfix(VRRig __instance, int soundIndex)
-        {
+        {          
             if (__instance.isOfflineVRRig)
             {
                 Debug.Log(soundIndex);
-                if (!( soundIndex == 67 || soundIndex == 66))
+                if (!(soundIndex == 67 || soundIndex == 66 || soundIndex == 270))
                 {
                     stepsCount++;
-                    Plugin.asset.GetComponentInChildren<Text>().text = $"{stepsCount}";
+                    Plugin.steps_text.GetComponent<Text>().text = $"{stepsCount}";
                     if (stepsCount % 10 == 0) { DataSystem.SaveData(); }
                 }
             }
         }
     }
 }
+
--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,9 @@
 ﻿using BepInEx;
 using GorillaSteps.Patches;
 using GorillaSteps.Scripts;
+using Photon.Pun;
 using System;
+using System.Collections;
 using System.IO;
 using System.Reflection;
 using UnityEngine;
@@ -14,9 +16,28 @@ namespace GorillaSteps
     [BepInPlugin("bjrsinge.gorillasteps", "GorillaSteps", "1.0.7")]
     public class Plugin : BaseUnityPlugin
     {
-        public static GameObject asset;
-        public bool init;
-        public GameObject btn;
+        public GameObject asset;
+        public static GameObject steps_text;
+        public GameObject stats;
+        public GameObject screen;
+        public GameObject time_text;
+        public GameObject code_text;
+        public GameObject date_text;
+        public GameObject playtime_text;
+        public static bool init;
+        public bool load;
+        public bool can_load = true;
+        public bool left_btn;
+        public bool right_btn;
+        public string date;
+        public string time;
+        public string code;    
+        public string playtime_string;     
+        public int playtime = 0;
+        public int seconds = 0;
+        public int minutes = 0;
+        public int hours = 0;
+        public int days = 0;
 
         public AssetBundle LoadAssetBundle(string path)
         {
@@ -37,20 +58,60 @@ namespace GorillaSteps
             PlayerData playerData = DataSystem.GetPlayerData();
             HandTapPatch.stepsCount = playerData.steps;
 
-            var bundle = LoadAssetBundle("GorillaSteps.watchbundleagain");
-            asset = Instantiate(bundle.LoadAsset<GameObject>("thewatch3"));
+            var bundle = LoadAssetBundle("GorillaSteps.newestwatch");
+            asset = Instantiate(bundle.LoadAsset<GameObject>("thewatch6"));
 
-            asset.transform.SetParent(GorillaTagger.Instance.rightHandTransform, false);
-            asset.AddComponent<MeshRenderer>();
-            asset.GetComponent<MeshRenderer>().enabled = true;
+            //objects
+            screen = asset.transform.Find("Cube (1)").gameObject;
+            steps_text = screen.transform.Find("steps_text").gameObject;
+            stats = screen.transform.Find("stats").gameObject;
+            time_text = stats.transform.Find("time").gameObject;
+            code_text = stats.transform.Find("code").gameObject;
+            playtime_text = stats.transform.Find("playtime").gameObject;
+            date_text = stats.transform.Find("date").gameObject;
+
+            //watch position
             asset.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
             asset.transform.localPosition = new Vector3(0.13f, -0.02f, -0.05f);
             asset.transform.localEulerAngles = new Vector3(GorillaLocomotion.Player.Instance.rightHandFollower.transform.rotation.x + 30f, 180f, 0f);
-            asset.GetComponentInChildren<BoxCollider>().enabled = false;
+            asset.transform.SetParent(GorillaTagger.Instance.rightHandTransform, false);
+            //screen.GetComponentInChildren<Text>().transform.localEulerAngles = new Vector3(0f, 90f, -90f);
 
-            asset.GetComponentInChildren<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
-            asset.GetComponentInChildren<Text>().text = playerData.steps.ToString();
-            asset.GetComponentInChildren<Text>().transform.localEulerAngles = new Vector3(0f, 90f, -90f);
+            //set fonts
+            steps_text.GetComponent<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
+            time_text.GetComponent<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
+            code_text.GetComponent<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
+            playtime_text.GetComponent<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
+            date_text.GetComponent<Text>().font = GorillaTagger.Instance.offlineVRRig.playerText.font;
+
+            /*time_text.GetComponent<Text>().fontSize = 1;
+            code_text.GetComponent<Text>().fontSize = 1;
+            date_text.GetComponent<Text>().fontSize = 1;
+            playtime_text.GetComponent<Text>().fontSize = 1;
+            code_text.GetComponent<Text>().fontSize = 1;*/
+            
+
+            //set text
+            steps_text.GetComponent<Text>().text = playerData.steps.ToString();
+
+            stats.SetActive(false);
+            steps_text.SetActive(true);
+
+            StartCoroutine("Playtime"); 
+        }
+
+        void LoadStats(bool toggle)
+        {
+           if (!toggle)
+           {
+                stats.SetActive(true);
+                steps_text.SetActive(false);
+           }
+           else
+           {
+                stats.SetActive(false);
+                steps_text.SetActive(true);
+           }
         }
 
         public void OnDisable()
@@ -69,15 +130,48 @@ namespace GorillaSteps
             init = true;
         }
 
-        public static void BtnFunc()
+        void Update()
         {
-            if (asset.GetComponentInChildren<Text>().text != HandTapPatch.stepsCount.ToString())
+            right_btn = ControllerInputPoller.instance.rightControllerSecondaryButton;
+            //set stats
+            time = DateTime.Now.ToString("t");
+            date = DateTime.Now.ToString("d");
+            code = PhotonNetwork.CurrentRoom != null ? PhotonNetwork.CurrentRoom.Name : "None";
+            //change stats text
+            time_text.GetComponent<Text>().text = time;
+            date_text.GetComponent<Text>().text = date;
+            code_text.GetComponent<Text>().text = code;
+            playtime_text.GetComponent<Text>().text = playtime_string;
+
+            if (init)
             {
-                asset.GetComponentInChildren<Text>().text = "testing";
+                if (right_btn)
+                {
+                    if (can_load)
+                    {
+                        LoadStats(load);
+                        load = !load;
+                        can_load = false;
+                    }
+                }
+                else
+                {
+                    can_load = true;
+                }
             }
-            else if (asset.GetComponentInChildren<Text>().text != "testing")
+        }
+
+        IEnumerator Playtime()
+        {
+            while (true)
             {
-                asset.GetComponentInChildren<Text>().text = HandTapPatch.stepsCount.ToString();
+                yield return new WaitForSeconds(1f);
+                playtime += 1;
+                seconds = playtime % 60;
+                minutes = playtime / 60 % 60;
+                hours = playtime / 3600 % 24;
+                days = playtime / 86400 % 365;
+                playtime_string = $"{days}d:{hours}h:{minutes}m:{seconds}s";
             }
         }
     }
